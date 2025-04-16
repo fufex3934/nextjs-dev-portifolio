601f210fba4cf1721f8d8ab5636539e6397ad240
fufex3934 <fufawakgari174@gmail.com>
2025-04-16 14:09:28 +0300

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-red-500">Hello world</h1>
-    </div>
+    <section className="text-center mt-10">
+      <h1 className="text-4xl font-bold">Hi, I'm Fufa!</h1>
+      <p className="mt-4 text-lg text-gray-600">Software Developer | React | Next.js </p>
+    </section>
   );
 }
