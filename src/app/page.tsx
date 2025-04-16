e93951dc570e1560207017f787189845d3479eda
fufex3934 <fufawakgari174@gmail.com>
2025-04-16 14:05:25 +0300

    initial setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1 className="text-red-500">Hello world</h1>
     </div>
   );
 }
